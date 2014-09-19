2cea2dad9eabfe6ec2365930375ef65b20c43711
Shichao An <shichao.an@nyu.edu>
2014-09-19 10:53:40 -0700

    Added dush for cross-platform

---
+++ cross/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Install script commands to /usr/local/bin
+
+localbin='/usr/local/bin'
+script_name=$(basename $0)
+
+echo 'Make sure /usr/local/bin is added to PATH'
+for cmd in *;
+do
+    [ "$cmd" != "$script_name" ] && cp "$cmd" "$localbin/$cmd"
+done
